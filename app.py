e4f5c11302b519685149530951c91736ec1ea812
Gune Ko <93659082+Koko1324@users.noreply.github.com>
2024-08-12 02:55:08 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,26 +1,10 @@
-from dash import Dash, dcc, html, Input, Output
+import streamlit as st
 
 
-app = Dash(__name__)
+def main():
+    st.title("Hello World")
+    x = st.slider('x')
+    st.write(x, "x 2 = ", x * 2)
 
-app.layout = html.Div([
-    html.H6("Change the value in the text box to see callbacks in action!"),
-    html.Div([
-        "Input: ",
-        dcc.Input(id='my-input', value='initial value', type='text')
-    ]),
-    html.Br(),
-    html.Div(id='my-output'),
-
-])
-
-@app.callback(
-    Output(component_id='my-output', component_property='children'),
-    Input(component_id='my-input', component_property='value')
-)
-
-def update_output_div(input_value):
-    return f'Output: {input_value}'
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == "__main__":
+    main()
