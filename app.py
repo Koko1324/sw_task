80296e2fb595cdb98db28b59ae1a51fafd9d5f74
Gune Ko <93659082+Koko1324@users.noreply.github.com>
2024-08-13 02:45:33 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 import streamlit as st
-st.set_page_config(layout="wide")
-col1,col2,col3 = st.columns([1,1,1])
-with col2:
-    st.title("일정에 맞는 여행 코스를 짜드립니다")
-    st.link_button('label', 'app1.py', help=None, type="secondary", disabled=False, use_container_width=False)
+#st.set_page_config(layout="wide")
+#col1,col2,col3 = st.columns([1,1,1])
+#with col2:
+#    st.title("일정에 맞는 여행 코스를 짜드립니다")
+opt = option_menu("MENU", ["세부 조회", "키워드 조회"],
+            icons=['bi bi-search', 'bi bi-zoom-in'],
+            menu_icon="bi bi-three-dots", default_index=0,
+             styles={
+        "container": {"padding": "5!important", "background-color": "#575757"},
+        "icon": {"color": "white", "font-size": "25px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "grey"},
+        "nav-link-selected": {"background-color": "#00ddff"},
+        }
+        )
+if opt == "세부 조회":
+            st.write('hello')
+if opt == "키워드 조회":
+            st.write("hi")
