ac9bd36b4ae8566f040743ebe40e64dabf8600aa
Gune Ko <93659082+Koko1324@users.noreply.github.com>
2024-08-13 02:25:13 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.set_page_config(layout="wide")
-col1,col2 = st.columns([1,1])
-with col1:
+col1,col2,col3 = st.columns([1,1,1])
+with col2:
     st.title("일정에 맞는 여행 코스를 짜드립니다")
