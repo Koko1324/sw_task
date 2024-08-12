cdda840520928534f5a81152f9c947b927a27a16
Gune Ko <93659082+Koko1324@users.noreply.github.com>
2024-08-13 02:30:33 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ st.set_page_config(layout="wide")
 col1,col2,col3 = st.columns([1,1,1])
 with col2:
     st.title("일정에 맞는 여행 코스를 짜드립니다")
-    st.link_button('label', 'app1.py', *, help=None, type="secondary", disabled=False, use_container_width=False)
+    st.link_button('label', 'app1.py', help=None, type="secondary", disabled=False, use_container_width=False)
